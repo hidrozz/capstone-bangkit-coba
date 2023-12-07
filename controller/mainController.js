6a7c5f92ad075f6400d8125ec4940d1154f01193
hidrozz <hidayat.nur2402@mail.ugm.ac.id>
2023-12-07 23:18:38 +0700

    j

--- controller/mainController.js
+++
@@ -1,9 +0,0 @@
-// controller/mainController.js
-const { firestore } = require('../config/firebaseConfig');
-
-const getHelloMessage = async () => {
-  const doc = await firestore.collection('messages').doc('hello').get();
-  return doc.data().message;
-};
-
-module.exports = { getHelloMessage };
--- middleware/loggingMiddleware.js
+++
@@ -1,8 +0,0 @@
-// middleware/loggingMiddleware.js
-const loggingMiddleware = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-  };
-  
-module.exports = loggingMiddleware;
-  
